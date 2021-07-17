acd1b0faf6a2b31b80ef2433b80ae3af8adf2d61
yuranski <yuriibidniak@gmail.com>
2021-07-17 17:36:26 +0300

    Full DZ 3

--- Lesson3/additional/main.js
+++ Lesson3/additional/main.js
@@ -1,11 +1,29 @@
-// ---------------ДОП---------------
-//
+
 //     -   функція Приймає масив та число "i", та міняє місцями об`єкт який знаходиться в індексі "i" на "i+1"
 //   EXAMPLE:
 //   foo([9,8,0,4], 0) // ==> [ 8, 9, 0, 4 ]
 //   foo([9,8,0,4], 1) // ==> [ 9 ,0, 8, 4 ]
 //   foo([9,8,0,4], 2) // ==> [ 9, 8, 4, 0 ]
-//
+let mas = [9, 8, 0, 4];
+let changeMas = function (mas, num) {
+    let ii = 0;
+    let mas2 = [];
+    for (let i = 0; i < mas.length; i++) {
+        if (mas[num] === mas[i]) {
+            mas2 += mas[i + 1];
+            ii++;
+        } else if (ii === 1) {
+            mas2 += mas[i - 1];
+            ii = 0;
+        } else {
+            mas2 += mas[i];
+        }
+    }
+    return mas2;
+}
+console.log(changeMas(mas, 2));
+
+
 // - Дано список імен.
 // let n1 = '    Harry       Potter      '
 // let n2 = '    Ron       Whisley      '
@@ -14,10 +32,68 @@
 // let n1 = 'Harry Potter'
 // let n2 = 'Ron Whisley'
 // let n3 = 'Hermione Granger'
-//
+let n1 = '    Harry       Potter      '
+let n2 = '    Ron       Whisley      '
+let n3 = '    Hermione       Granger      '
+let normalized = function (name) {
+    let res = name.replaceAll(' ', '');
+    return res.replace(/([A-Z])/g, ' $1').trim();
+}
+console.log(normalized(n1));
+console.log(normalized(n2));
+console.log(normalized(n3));
+
+
 // - Сворити функцію яка буде переносити елементи з значенням 0 у кінець маисву. Зберігаючи при цьому порядок не нульових значень.
 // Двожина масиву від 2 до 100
 // EXAMPLE:
 // [1,0,6,0,3] => [1,6,3,0,0]
 // [0,1,2,3,4] => [1,2,3,4,0]
 // [0,0,1,0]   => [1,0,0,0]
+let exFinal1 = [1,0,6,0,3];
+let exFinal2 = [0,1,2,3,4];
+let exFinal3 = [0,0,1,0];
+let zeroToTheEnd = function (arr){
+    let newArr = [];
+    let ex = [];
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i] === 0) {
+            ex += 0;
+        }
+        else {
+            newArr += arr[i];
+        }
+    }
+    for (let i = 0; i < ex.length; i++) {
+        newArr += 0;
+    }
+    return newArr;
+}
+console.log(zeroToTheEnd(exFinal1));
+console.log(zeroToTheEnd(exFinal2));
+console.log(zeroToTheEnd(exFinal3));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Lesson3/homework/main.js
+++ Lesson3/homework/main.js
@@ -1,9 +1,72 @@
-
 // - створити функцію яка обчислює та повертає площу прямокутника висотою
+let areaRectangle = function (a, b) {
+    return a + b;
+}
+
 // - створити функцію яка обчислює та повертає площу кола
+let areaCircle = function (r) {
+    return 3.1415 * (r * r);
+}
+
 // - створити функцію яка обчислює та повертає площу циліндру
+let areaCylinder = function (r, h) {
+    return 2 * 3.1415 * r * (r + h);
+}
+
 // - створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше (Math використовувати заборонено);
+let someMas = [4,2,3,4,5,6,7,5,4,3,2,5,5,6,7,6];
+let some = function (a) {
+    let max = 0;
+    let min = 10000000000000;
+    for (let i = 0; i < a.length; i++) {
+        if(a[i] < min){min = a[i];}
+        if (a[i] > max){max = a[i];}
+    }
+    console.log(max);
+    return min;
+}
+
+
 // - створити функцію яка при створює блок з текстом. Текст задати через аргумент
+let someTxt = '123';
+let createDiv = function (txt){
+    document.write(`<div>${txt}</div>`)
+}
+createDiv(someTxt);
+
+
 // - створити функцію яка при створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
-// - створити функцію яка при створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
+someTxt = '123';
+let createLi = function (txt){
+    document.write(`<ul>
+                        <li>${txt}</li>
+                        <li>${txt}</li>
+                        <li>${txt}</li>
+                    </ul>`)
+}
+createLi(someTxt);
+
+
+// - створити функцію яка при створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий.
+// Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
+someTxt = '123';
+let createLiMore = function (txt, createLi){
+    document.write(`<ul>`);
+    for (let i = 0; i < createLi; i++) {
+        document.write(`<li>${txt}</li>`);
+    }
+    document.write(`</ul>`);
+}
+createLiMore(someTxt, 20);
+
+
 // - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
+someTxt = ['123',2,true,false,56456,'Dasha'];
+let createLiMore2 = function (txt, createLi){
+    document.write(`<ul>`);
+    for (let i = 0; i < createLi.length; i++) {
+        document.write(`<li>${txt[i]}</li>`);
+    }
+    document.write(`</ul>`);
+}
+createLiMore2(someTxt, someTxt);
